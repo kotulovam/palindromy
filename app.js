56576396aa6102e4942db252e42fa8e811563021
Michaela Kotuľová <kotulova.michaela271@gmail.com>
2025-04-21 18:20:53 +0200

    Solution

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+  return text.split('').reverse().join('');
+};
 
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+const slova = document.querySelectorAll('li');
+
+slova.forEach((slovo) => {
+  const text = slovo.textContent.toLowerCase();
+  const textBezMezer = text.replace(/\s/g, '');
+  if (textBezMezer === textPozpatku(textBezMezer))
+    slovo.classList.add('palindrom');
+});
